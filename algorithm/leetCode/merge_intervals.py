f6f22aad76ee8798c1682343debab2ea801cf116
dictator-x <dictator.xwu@gmail.com>
2020-02-12 10:17:10 -0500

    leetCode 56.Merge Intervals

---
+++ algorithm/leetCode/merge_intervals.py
@@ -0,0 +1,25 @@
+# leetCode: 56.Merge Intervals
+
+from typing import List
+
+def merge(intervals: List[List[int]]) -> List[List[int]]:
+    # sort list
+    sorted_intervals = sorted(intervals, key=lambda interval: interval[0])
+
+    # do merge
+    result_intervals = []
+    for interval in sorted_intervals:
+        if not result_intervals:
+            result_intervals.append(interval)
+        else:
+            current_last_interval = result_intervals[-1]
+            if current_last_interval[1] >= interval[0]:
+                current_last_interval[1] = interval[1]
+            else:
+                result_intervals.append(interval)
+
+    return result_intervals
+
+if __name__ == "__main__":
+    print (merge([[15,18], [1,3], [2,6], [8,10]]))
+    print (merge([[1,4],[4,5]]))

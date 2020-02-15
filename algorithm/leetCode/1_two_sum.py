bb66e882ba2ec84d50917893a41aa7504453af63
dictator-x <dictator.xwu@gmail.com>
2020-02-14 22:39:43 -0500

    leetCode:1_two_sum

---
+++ algorithm/leetCode/1_two_sum.py
@@ -0,0 +1,19 @@
+#1. Two Sum
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # use map record looped value.
+        record = {}
+
+        for i, n in enumerate(nums):
+            remain = target - n
+            if remain not in record:
+                record[n] = i
+            else:
+                return [record[remain], i]
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.twoSum([2, 7, 11, 15], 9))

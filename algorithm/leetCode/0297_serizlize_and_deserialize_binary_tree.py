9c955c8310ae8e5c5084bfc99d9e8fe2b1139f36
dictator-x <dictator.xwu@gmail.com>
2020-03-17 15:25:00 -0400

    leetCode: 297. Serialize and Deserialize Binary Tree

---
+++ algorithm/leetCode/0297_serizlize_and_deserialize_binary_tree.py
@@ -0,0 +1,37 @@
+"""
+297. Serialize and Deserialize Binary Tree
+"""
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        ret = []
+
+        def doSerialize(root):
+            if root == None:
+                ret.append("None")
+                return
+
+            ret.append(str(root.val))
+            doSerialize(root.left)
+            doSerialize(root.right)
+
+        doSerialize(root)
+        return ",".join(ret)
+
+    def deserialize(self, data):
+        data = [ ( int(i) if i != "None" else None ) for i in data.split(",") ]
+        def doDeserialize(data):
+            val = data.pop(0)
+            if val == None:
+                return None
+            else:
+                node = TreeNode(val)
+                node.left = doDeserialize(data)
+                node.right = doDeserialize(data)
+                return node

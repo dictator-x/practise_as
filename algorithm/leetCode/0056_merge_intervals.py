5432adb7ca65720d87e8d0ed6955afa1dcbb5046
dictator-x <dictator.xwu@gmail.com>
2020-03-21 14:35:04 -0400

    fix bug on leetCode: 56. Merge Intevals

--- algorithm/leetCode/0056_merge_intervals.py
+++ algorithm/leetCode/0056_merge_intervals.py
@@ -1,24 +1,26 @@
-# leetCode: 56.Merge Intervals
+"""
+56. Merge Intervals
+"""
 
 from typing import List
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:    # sort list
+        sorted_intervals = sorted(intervals, key=lambda interval: interval[0])
 
-def merge(intervals: List[List[int]]) -> List[List[int]]:
-    # sort list
-    sorted_intervals = sorted(intervals, key=lambda interval: interval[0])
-
-    # do merge
-    result_intervals = []
-    for interval in sorted_intervals:
-        if not result_intervals:
-            result_intervals.append(interval)
-        else:
-            current_last_interval = result_intervals[-1]
-            if current_last_interval[1] >= interval[0]:
-                current_last_interval[1] = interval[1]
-            else:
+        # do merge
+        result_intervals = []
+        for interval in sorted_intervals:
+            if not result_intervals:
                 result_intervals.append(interval)
+            else:
+                current_last_interval = result_intervals[-1]
+                if current_last_interval[1] >= interval[0]:
+                    if current_last_interval[1] < interval[1]:
+                        current_last_interval[1] = interval[1]
+                else:
+                    result_intervals.append(interval)
 
-    return result_intervals
+        return result_intervals
 
 if __name__ == "__main__":
     print (merge([[15,18], [1,3], [2,6], [8,10]]))

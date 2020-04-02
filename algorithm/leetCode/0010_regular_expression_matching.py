bb932a28ef9563a1a634206d367d9e679f8c8d58
dictator-x <dictator.xwu@gmail.com>
2020-04-01 20:42:19 -0400

    add tips on regular expression matching

--- algorithm/leetCode/0010_regular_expression_matching.py
+++ algorithm/leetCode/0010_regular_expression_matching.py
@@ -7,12 +7,23 @@ class Solution:
         s_l = len(s)
         p_l = len(p)
 
+        # create 2d metrix with extra 1 length, because will need to consider
+        # empty string case.
         records = [ [ False for _ in range (s_l + 1) ] for _ in range (p_l + 1) ]
 
+        # if pattern == "" and string == "", then the result should be true
         records[0][0] = True
 
+        # compare empty string with any non-empty pattern, the result will always be false
+        # compare pattern with empty string
         for i in range(1, p_l + 1):
+            # "*" will not be at the beginning of the pattern.
             if ( p[i-1] == "*" ):
+                # why it is based on i-2 position?
+                # i-1 position contain char in front of *, like a* or .*
+                # The current value really depends on anything pattern before .* or a*
+                # eg: aa* will be false, because pattern a not match with empty string
+                # eg: a*a* will be true, because pattern a* match with empty string
                 records[i][0] = records[i-2][0]
 
         for i in range(1, p_l + 1):
@@ -23,6 +34,7 @@ class Solution:
                     if s[j-1] == p[i-2] or p[i-2] == ".":
                         records[i][j] = (records[i-1][j] or records[i][j-1] or records[i-2][j])
                     else:
+                        # pattern (...)a* vs string (...)
                         records[i][j] = records[i-2][j]
 
         return records[p_l][s_l]

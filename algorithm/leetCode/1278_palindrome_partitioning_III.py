b4e7ffdb365fd7e5b59295ebe8c304de5182a8ca
dictator-x <dictator.xwu@gmail.com>
2020-05-11 16:17:05 -0400

    leetCode: 1278. refactor and improve performance

--- algorithm/leetCode/1278_palindrome_partitioning_III.py
+++ algorithm/leetCode/1278_palindrome_partitioning_III.py
@@ -8,10 +8,16 @@ class Solution:
         n = len(s)
         cost = [ [0] * n for _ in range(n) ]
 
+        # key: under stand this.
+        # for l in range(2, n+1):
+        #     for i in range(0, n):
+        #         for j in range(i+1, n):
+        #             cost[i][j] = cost[i+1][j-1] + (1 if s[i] != s[j] else 0)
+
         for l in range(2, n+1):
-            for i in range(0, n):
-                for j in range(i+1, n):
-                    cost[i][j] = cost[i+1][j-1] + (1 if s[i] != s[j] else 0)
+            for i in range(0, n-l+1):
+                j = i + l - 1
+                cost[i][j] = cost[i+1][j-1] + (1 if s[i] != s[j] else 0)
 
         dp = [ [200//2] * (n) for _ in range(k+1) ]
 
